1ac78f9a4887f3d7c7b4e1dfabb7d11a39229e04
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-12-11 11:53:08 +0100

    notes adding

--- src/main.js
+++ src/main.js
@@ -2,20 +2,77 @@ let amountSemesters = 0;
 let amountAverage = 0;
 
 export function addSem() {
+  console.log("function called");
+  amountSemesters++;
 
-    amountSemesters ++;
+  let templateSem = document
+    .querySelector("#template_semester")
+    .content.cloneNode(true);
 
-    let templateSem = document.querySelector("#template_semester").content.cloneNode(true);
+  let divSem = document.querySelector(".div_sem");
 
-    let divSem = document.querySelector(".div_sem")
+  divSem.appendChild(templateSem);
 
-    divSem.appendChild(templateSem)
+  let sem = divSem.lastElementChild;
 
-    let sem = divSem.lastElementChild;
+  let semName = sem.querySelector("dt");
+  semName.innerText = "Semester " + amountSemesters;
 
-    let semName = sem.querySelector("dt")
-    semName.innerText = "Semestre " + amountSemesters;
+  let addNote = sem.querySelector("button");
+  let noteInput = sem.querySelector("input");
 
+  addNote.addEventListener("click", function (event) {
+    nouvelleNote(noteInput.value, sem);
+    noteInput.value = "";
+  });
+
+  noteInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      nouvelleNote(noteInput.value, sem);
+      noteInput.value = "";
+    }
+  });
 }
 
-addSem()
+function nouvelleNote(note, semestre) {
+  console.log(note);
+
+  let noteStock = semestre.querySelector("dd > div");
+
+  let templateDot;
+  if (note < 4) {
+    templateDot = document
+      .querySelector("#red-dot-svg")
+      .content.cloneNode(true);
+  } else if (note > 4) {
+    templateDot = document
+      .querySelector("#green-dot-svg")
+      .content.cloneNode(true);
+  } else {
+    templateDot = document
+      .querySelector("#orange-dot-svg")
+      .content.cloneNode(true);
+  }
+
+  let templateNote = document
+    .querySelector("#noteSpan")
+    .content.cloneNode(true);
+
+  let span = templateNote.querySelector("span");
+  span.textContent = note;
+
+  // Add the dot directly within the span without changing HTML
+  let dot = templateDot.querySelector("svg");
+  span.insertBefore(dot, span.firstChild);
+
+  noteStock.appendChild(templateNote);
+}
+
+addSem();
+
+let addSemButton = document.querySelector("#add_sem");
+addSemButton.addEventListener("click", function (event) {
+  if (amountSemesters < 8) {
+    addSem();
+  }
+});
